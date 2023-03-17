46893bd168e0785db661a36626cb364c3b33e791
MaxSchemenauer <113937793+MaxSchemenauer@users.noreply.github.com>
2023-03-16 19:20:07 -0700

    trash flow improved

--- src/Trash.java
+++ src/Trash.java
@@ -24,15 +24,22 @@ public class Trash extends Animate{
     public boolean nextPositionTrash(WorldModel world, ImageStore imageStore,EventScheduler scheduler) {
         Point down = new Point(getPosition().x, getPosition().y + 1);
         Point right = new Point(getPosition().x + 1, getPosition().y);
+        Point up = new Point(getPosition().x, getPosition().y - 1);
 
-        if(world.withinBounds(down) && world.getOccupancyCell(down) != null && world.getOccupancyCell(down).getClass() == Water.class) {
+
+        if(world.withinBounds(right) && world.getOccupancyCell(right) != null && world.getOccupancyCell(right).getClass() == Water.class) {
             if((int) (Math.random() * 10) == 5) {
+                world.swapEntity(this, world.getOccupancyCell(right));
+            }
+        }
+        if(world.withinBounds(down) && world.getOccupancyCell(down) != null && world.getOccupancyCell(down).getClass() == Water.class) {
+            if((int) (Math.random() * 20) == 5) {
                 world.swapEntity(this, world.getOccupancyCell(down));
             }
         }
-        else if(world.withinBounds(right) && world.getOccupancyCell(right) != null && world.getOccupancyCell(right).getClass() == Water.class) {
-            if((int) (Math.random() * 10) == 5) {
-                world.swapEntity(this, world.getOccupancyCell(right));
+        else if(world.withinBounds(up) && world.getOccupancyCell(up) != null && world.getOccupancyCell(up).getClass() == Water.class) {
+            if((int) (Math.random() * 100) == 5) {
+                world.swapEntity(this, world.getOccupancyCell(up));
             }
         }
         return true;
